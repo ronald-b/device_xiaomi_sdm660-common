a51335d6dfda3da796127091f9bd9b64044caa5e
Arian <arian.kulmer@web.de>
2021-04-27 16:14:58 +0600

    sdm660-common: Build antradio_app instead of com.dsi.ant.antradio_library
    Change-Id: Icc3c562bf281d4f359b51e58a2ef53853b71e4f7

--- sdm660.mk
+++ sdm660.mk
@@ -134,11 +134,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant@1.0.vendor \
-    com.dsi.ant.antradio_library
-
-# ANT Permission
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app
 
 # AuthSecret
 PRODUCT_PACKAGES += \
