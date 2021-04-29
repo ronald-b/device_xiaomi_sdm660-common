7639aae072569b3035939040bd1c83bd230071a4
tesla59 <nishant.singh@pixysos.com>
2021-04-30 00:36:20 +0600

    sdm660-common: Uprev RIL interfaces - uprev factory manifest entry to 2.2 - uprev imsRadio HAL to latest version 1.7 - add package android.hardware.radio.deprecated - add vendor.qti.hardware.radio.deviceinfo - uprev QtiRadio HAL to v2.6 - remove duplicate QtiRadio manifest-entry

--- sdm660.mk
+++ sdm660.mk
@@ -490,7 +490,8 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
-    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.2 \
     rild \
     libavservices_minijail.vendor \
