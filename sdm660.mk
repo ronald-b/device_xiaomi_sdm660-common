fd77e85f79a8bb41e93549bdd87b3246f8240dfe
OdSazib <odsazib@gmail.com>
2021-05-03 13:06:04 +0600

    sdm660-common: Fix OVERRIDE_QCOM_HARDWARE_VARIANT for ProductConfig

--- sdm660.mk
+++ sdm660.mk
@@ -24,6 +24,7 @@
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := sdm660
+OVERRIDE_QCOM_HARDWARE_VARIANT := sdm660
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
